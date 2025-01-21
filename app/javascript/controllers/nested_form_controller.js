3d0ac279b0b42c3945bea2cd8e693c2d6931f597
Bruno Pellizzetti <pelli.br@gmail.com>
2025-01-20 21:31:03 -0300

    improve css

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,22 +1,27 @@
+// app/javascript/controllers/nested_form_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["template", "fields"]
+  static targets = ["fields", "template"]
 
   add(event) {
     event.preventDefault()
+
+    // Replace 'NEW_RECORD' with a unique timestamp to avoid conflicts
     const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
-    this.fieldsTarget.insertAdjacentHTML('beforeend', content)
+    this.fieldsTarget.insertAdjacentHTML("beforeend", content)
   }
 
   remove(event) {
     event.preventDefault()
-    const wrapper = event.target.closest('.nested-fields')
-    if (wrapper.dataset.newRecord == 'true') {
-      wrapper.remove()
+
+    const wrapper = event.target.closest(".nested-fields")
+    if (wrapper.dataset.newRecord === "true") {
+      wrapper.remove() // Remove new records directly
     } else {
-      wrapper.style.display = 'none'
-      wrapper.querySelector("input[name*='_destroy']").value = 1
+      // Mark existing records for destruction
+      wrapper.querySelector("input[name*='_destroy']").value = "1"
+      wrapper.style.display = "none" // Hide the field
     }
   }
 }
